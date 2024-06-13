a455a4799d0cc5c153d284027f10d785941a90ba
SeSacjh <anms170405@gmail.com>
2024-06-13 23:57:57 +0900

    개인프로젝트 업데이트

--- lecture/11-js/lesson/lesson01.js
+++ lecture/11-js/lesson/lesson01.js
@@ -8,6 +8,48 @@ class Shape {
         return result;
     }
 }
+// let rec1 = new Shape(3,4);
+// console.log(rec1.getArea());
+// ------------------------------------------------------
+class Rectangel extends Shape {
+    constructor (width, height) {
+        super(width,height)
+    }
+    getArea() {
+        return this.width * this.height;
+    }
 
-let rec1 = new Shape(3,4);
-console.log(rec1.getArea());
+    getSqrt() {
+        return Math.sqrt(this.width**2 + this.height**2)
+    }
+}
+
+// ------------------------------------------------------
+class Triangle {
+    constructor (width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return (this.width * this.height) / 2;
+    }
+}
+
+// ------------------------------------------------------
+class Circle extends Shape {
+    constructor (width, height, radius) {
+        super(width, height)
+        this.radius = radius;
+    }
+    getArea() {        
+        let area = this.radius * this.radius * Math.PI;
+        return area;
+    }
+}
+
+let rec = new Rectangel(2, 4);
+let tri = new Triangle(5, 2);
+let cir = new Circle(4, 4, 3);
+console.log(rec.getArea());
+console.log(tri.getArea());
+console.log(cir.getArea());
--- lecture/11-js/lesson/lesson03.js
+++ lecture/11-js/lesson/lesson03.js
@@ -1,11 +1,26 @@
-class Shape {
-    constructor (inch) {
-        this.inch = inch;        
+class Rectangel {
+    constructor (width, height) {
+        this.width = width;
+        this.height = height;
     }
     getArea() {
-        return Math.sqrt(this.inch**2);
+        return this.width * this.height;
     }
 }
 
-let rec1 = new Shape(4);
+let rec1 = new Rectangel(4,4);
 console.log(rec1.getArea());
+
+
+class Triangle {
+    constructor (width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    getArea() {
+        return (this.width * this.height) / 2;
+    }
+}
+
+let tri1 = new Triangle(4,5);
+console.log(tri1.getArea());
