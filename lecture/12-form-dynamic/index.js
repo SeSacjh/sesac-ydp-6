afed207e2e81f88252f89da653e3c4e432ec72a5
SeSacjh <anms170405@gmail.com>
2024-06-17 16:15:44 +0900

    study: http 비동기 통신 -ajax

---
+++ lecture/12-form-dynamic/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const PORT = 8888;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// ex. body-parser 모듈
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('dynamic', { title: '동적 폼을 배워보자!'});
+})
+
+// ajax
+app.get('/ajax', (req, res) => {
+    // GET 방식이므로 브라우저에서 URL 쿼리까지 직접 입력해서도 값을 확인 가능
+    // ex. http://localhost:8888/ajax?name=123&gender=남자
+    console.log(req.query);
+    res.send(req.query);
+})
+
+app.post('/ajax', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
+app.listen(PORT, () => {
+    console.log(`Start server!!! http://localhost:${PORT}`);
+})
