5dfd22427166a068481e935b4cf1857baa83bc6f
SeSacjh <anms170405@gmail.com>
2024-06-18 22:33:31 +0900

    study: 비동기 처리 실습

--- lecture/test/app.js
+++ lecture/test/app.js
@@ -1,14 +1,23 @@
-const express = require('express');
-const app = express();
-const PORT = 8000;
+let product, price;
 
-app.set('view engine','ejs')
-app.set('views','./views')
+function goMart() {
+  console.log(`마트에 가서 어떤 음료를 살지 고민한다..`);
+}
 
-app.get('/', (req,res) => {
-    res.render('index')
-})
+function pickDrink(callback) {
+  setTimeout(function () {
+    console.log(`고민 끝`);
+    product = `제로콜라`;
+    price = `3000원`;
+    callback();  // pickDrink가 완료된 후에 콜백 함수 호출
+  }, 3000);
+}
 
-app.listen(PORT, () => {
-    console.log('8000번 포트에서 실행');
-})
+function pay() {
+  setTimeout(function () {
+    console.log(`상품명: ${product} // 가격: ${price}`);
+  }, 5000);
+}
+
+goMart();
+pickDrink(pay);
