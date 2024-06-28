acf4eba05114300d2660458a1a817923220a8bb9
SeSacjh <anms170405@gmail.com>
2024-06-28 16:10:09 +0900

    study: sequelize model define

---
+++ lecture/18-sequelize/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const router = require('./routes/index');
+const { sequelize } = require('./models');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.use('/', router);
+
+sequelize
+    .sync({ force: true })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log('Database connection succeeded!')
+            console.log(`Server is running! Port number is ${PORT} ...`);
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+    });
